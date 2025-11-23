e9f61b12a4017c42ae9145a0d4d80da48587cd04
Michael Freimüller <michael@mfreimueller.com>
2025-11-23 10:41:36 +0100

    initial commit
    - add maven deps
    - add puml

---
+++ src/main/java/at/spengergasse/ivehif/december_probetest/Application.java
@@ -0,0 +1,13 @@
+package at.spengergasse.ivehif.december_probetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/ivehif/december_probetest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.ivehif.december_probetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
